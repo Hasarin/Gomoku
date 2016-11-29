d6e737015e465adb48b3c846dd1ed6d3a4b77992
murschet <murschet@meleze19.iut-infobio.priv.univ-lille1.fr>
2016-11-29 16:09:17 +0100

    Test couleur

--- Gomoku/src/gui/Mouse.java
+++ Gomoku/src/gui/Mouse.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -8,20 +9,30 @@ import java.util.List;
 import javax.management.ListenerNotFoundException;
 
 import jeu.Coordonée;
+import jeu.Joueurs;
 
 public class Mouse implements MouseListener {
 
 	private List<Coordonée> liste = new ArrayList<Coordonée>();
-
-	public Mouse(List liste) {
+	private Joueurs joueur1;
+	private Plateau plateau;
+	public Mouse(List liste, Plateau plateau, Joueurs joueur1) {
 		this.liste = liste;
+		this.plateau = plateau;
+		this.joueur1 = joueur1;
+		
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		for (Coordonée c : liste) {
 			  if(c.getX()-10 < e.getX() && c.getX()+10 > e.getX()){
 				  if(c.getY()-10 < e.getY() && c.getY()+10 > e.getY()){
-					  System.out.println("toucher");
+					  
+					
+					  plateau.getGraphics().fillOval(c.getX()-10,c.getY()-10, 20, 20);
+					  
+					  
+					  
 					  
 					  
 					  }
--- Gomoku/src/gui/Plateau.java
+++ Gomoku/src/gui/Plateau.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,54 +13,94 @@ import javax.swing.JPanel;
 import jeu.Coordonée;
 import jeu.Joueurs;
 
-public class Plateau extends JPanel {
+public class Plateau extends JPanel implements MouseListener {
 
 	private Mouse mouse;
 	public List<Coordonée> liste = new ArrayList<Coordonée>();
-	int taille ;
+	int taille;
+
 	public Plateau(int taille, Joueurs joueur1, Joueurs joueur2) {
 		this.taille = taille;
-		mouse = new Mouse(liste);
-		this.addMouseListener(mouse);
-			
-			
+
+		addMouseListener(this);
+
 	}
 
 	public void paintComponent(Graphics g) {
 		g.setColor(Color.ORANGE);
 		g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
 		g.setColor(Color.BLACK);
-		
+
 		for (int i = 0; i < taille; i++) {
 			for (int y = 0; y < taille; y++) {
-				Coordonée c = new Coordonée(i*40+20, y*40+10);
-				Coordonée c2 = new Coordonée(i*40+20, y*40+10+40);
-				g.drawRect(i * 40 + 20, y*40+10, 40, 40);
+				Coordonée c = new Coordonée(i * 40 + 20, y * 40 + 10);
+				Coordonée c2 = new Coordonée(i * 40 + 20, y * 40 + 10 + 40);
+				g.drawRect(i * 40 + 20, y * 40 + 10, 40, 40);
 				Boolean b = false;
 				for (Coordonée coor : liste) {
-					if(c.compare(coor)){
+					if (c.compare(coor)) {
 						b = true;
 					}
 				}
-				if(!b){
+				if (!b) {
 					liste.add(c);
 					liste.add(c2);
 				}
-				
+
 			}
-			liste.add(new Coordonée(taille*40+20, i*40+10));
-			liste.add(new Coordonée(i*40+20, taille*40+10));
-			
+			liste.add(new Coordonée(taille * 40 + 20, i * 40 + 10));
+			liste.add(new Coordonée(i * 40 + 20, taille * 40 + 10));
 
 		}
-		liste.add(new Coordonée(taille*40+20, taille*40+10));
+		liste.add(new Coordonée(taille * 40 + 20, taille * 40 + 10));
 		for (Coordonée coordonée : liste) {
-			g.fillRect(coordonée.getX()-5, coordonée.getY()-5, 10, 10);
+			g.fillRect(coordonée.getX() - 5, coordonée.getY() - 5, 10, 10);
 		}
 		
+
 	}
 	
+
 	public List getListe() {
 		return liste;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		for (Coordonée c : liste) {
+			if (c.getX() - 10 < e.getX() && c.getX() + 10 > e.getX()) {
+				if (c.getY() - 10 < e.getY() && c.getY() + 10 > e.getY()) {
+					
+					getGraphics().fillOval(c.getX() - 10, c.getY() - 10, 20, 20);
+
+				}
+			}
+
+		}
+	}
+
+	
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
